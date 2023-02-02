50fe8764562e59583106d13d154fd7b1eb55dab7
Jakob Rathberger <jakob.rathberger@gmail.com>
2023-02-02 09:00:01 +0100

    #leoiot2-11 change mqtt server to new secure mqtt server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   vmUrl: 'https://vm139.htl-leonding.ac.at',
-  mqttUrl: 'vm90.htl-leonding.ac.at',
+  mqttUrl: 'mqtt.htl-leonding.ac.at',
   corePathPrefix: '',
   mqttPathPrefix: '/broker',
   mqttPath: '/mqtt',
-  mqttPort: 9001,
+  mqttPort: 5055,
   vmPort: 8080,
   pathPrefix: '/j.rathberger/frontend'
 };
